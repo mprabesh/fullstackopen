22aa543d1c0016f6807b746107b38af1b36a7b03
mprabesh <theprabesh723@gmail.com>
2023-07-20 23:05:50 +0545

    comments added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import Statistics from "./Statistics";
 
 const Headers = ({ text }) => {
+  // Headers Component wiht dynamic content
   return <h1>{text}</h1>;
 };
 
 const FeedbackButton = ({ changeFunc, text }) => {
+  // feedback component button with dynamic button label
   return <button onClick={changeFunc}>{text}</button>;
 };
 
 const App = () => {
+  //main component
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  let checkFeedBack = good || neutral || bad;
+  let checkFeedBack = good || neutral || bad; // check the feedback given or not to render no feedback condition
+
+  /* 3 stateupdate functions  */
 
   const addGood = () => {
     setGood(good + 1);
