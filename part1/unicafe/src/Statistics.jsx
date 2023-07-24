4499ee0ed9e32c86bd2b0574014cc2accd7eadba
mprabesh <theprabesh723@gmail.com>
2023-07-24 08:18:06 +0545

    positive percentage added

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -3,15 +3,16 @@ import StatisticLine from "./StatisticLine";
 const Statistics = ({ stats }) => {
   let total = stats.good + stats.bad + stats.neutral;
   const averageVal = (stats.good - stats.bad) / total;
-  const calPositive = stats.good / total;
+  const calPositive = (stats.good / total) * 100;
 
   return (
     <div>
       <StatisticLine text="good" value={stats.good} />
       <StatisticLine text="neutral" value={stats.neutral} />
       <StatisticLine text="bad" value={stats.bad} />
+      <StatisticLine text="all" value={total} />
       <StatisticLine text="average" value={averageVal} />
-      <StatisticLine text="all" value={String(calPositive) + "%"} />
+      <StatisticLine text="positive" value={String(calPositive) + "%"} />
     </div>
   );
 };
