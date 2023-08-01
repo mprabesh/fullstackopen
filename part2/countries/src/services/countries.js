3fed5bda00ffbc5d6be0988a6551c22617dbdea0
mprabesh <theprabesh723@gmail.com>
2023-08-01 11:00:16 +0545

    part2 countries project completed.

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const allCountriesURL = "https://studies.cs.helsinki.fi/restcountries/api/all";
-const API_KEY = "a3d79902f419b3a8154c88036e5a16a7";
-
 export const getAllCountries = () => {
-  return axios.get(allCountriesURL);
+  return axios.get(import.meta.env.VITE_API_ROUTE);
 };
 
 export const getWeatherData = (capital) => {
   return axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${
+      import.meta.env.VITE_API_KEY
+    }&units=metric`
   );
 };
