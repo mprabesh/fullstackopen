ca9b5d58242ac8398bc53a288ea41fb64f2608bc
mprabesh <theprabesh723@gmail.com>
2023-08-04 15:04:13 +0545

    fix: empty data prblm fixed

--- part3/index.js
+++ part3/index.js
@@ -20,17 +20,16 @@ app.get("/", (req, res) => {
   res.send("<h1>This is phonebook app.</h1>");
 });
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
   Phonebook.find({})
     .then((result) => {
       res.status(200).json(result);
     })
     .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+      next(err);
     });
 });
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const myId = req.params.id;
   Phonebook.findById(myId)
     .then((result) => {
@@ -41,7 +40,7 @@ app.get("/api/persons/:id", (req, res) => {
       }
     })
     .catch((err) => {
-      res.status(500).json({ error: err.name });
+      next(err);
     });
 });
 app.get("/info", (req, res) => {
@@ -53,13 +52,15 @@ app.get("/info", (req, res) => {
   });
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const newObj = req.body;
+  console.log(newObj);
   Phonebook.findOne({ name: newObj.name }).then((result) => {
     if (result) {
       res.status(403).json({ error: "name must be unique" });
     } else {
       if (newObj.name && newObj.number) {
+        console.log("apple");
         const contact = new Phonebook(newObj);
         contact
           .save()
@@ -67,23 +68,23 @@ app.post("/api/persons", (req, res) => {
             res.status(200).json(result);
           })
           .catch((err) => {
-            console.log(err);
+            next(err);
           });
       } else {
-        res.status(400).json({ error: "Missing content" });
+        res.status(400).json({ message: "Missing content" });
       }
     }
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   const myId = req.params.id;
   Phonebook.findByIdAndDelete(myId)
     .then((result) => {
       console.log(result);
       res.status(204).send("Deletetion successful");
     })
-    .catch((err) => console.log(err, "xxx"));
+    .catch((err) => next(err));
 });
 
 app.put("/api/persons/:id", (req, res) => {
@@ -97,6 +98,20 @@ app.put("/api/persons/:id", (req, res) => {
       console.log(err);
     });
 });
+
+const unknownEndPoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndPoint);
+
+const errrorHandler = (error, req, res, next) => {
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+};
+
+app.use(errrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
