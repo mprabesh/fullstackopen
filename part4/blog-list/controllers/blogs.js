ac0a8880b9f83558754d26fb061adbe3455f0389
mprabesh <theprabesh723@gmail.com>
2023-08-10 21:44:42 +0545

    feat: new endpoints added

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogRoute = require("express").Router();
 const Blog = require("../models/blogs");
 
-blogRoute.get("/", async (req, res, err) => {
+blogRoute.get("/", async (req, res, next) => {
   try {
     const response = await Blog.find({});
     res.status(200).json(response);
@@ -10,16 +10,46 @@ blogRoute.get("/", async (req, res, err) => {
   }
 });
 
-blogRoute.post("/", async (req, res, err) => {
+blogRoute.get("/:id", async (req, res, next) => {
+  const myId = req.params.id;
+  try {
+    const response = await Blog.findById(myId);
+    res.status(200).json(response);
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogRoute.post("/", async (req, res, next) => {
   if (!Object.keys(req.body).includes("likes")) {
     req.body["likes"] = 0;
   }
   const newBlog = new Blog(req.body);
 
   try {
-    console.log(req.body);
     const result = await newBlog.save();
     res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogRoute.delete("/:id", async (req, res, next) => {
+  const myID = req.params.id;
+  try {
+    await Blog.findByIdAndDelete(myID);
+    res.status(200).send("Deletion successful.");
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogRoute.put("/:id", async (req, res, next) => {
+  const myId = req.params.id;
+  const myUpdatedObj = req.body;
+  try {
+    const response = await Blog.findByIdAndUpdate(myId, myUpdatedObj);
+    res.status(200).json(response);
   } catch (err) {
     next(err);
   }
