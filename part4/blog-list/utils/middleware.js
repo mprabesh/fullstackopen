bd97137da95b326d444d866894aa55a09079700e
mprabesh <theprabesh723@gmail.com>
2023-08-17 09:14:27 +0545

    feat:minor edit for testing purpose

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -42,8 +42,9 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message });
   } else if (error.name === "TokenExpiredError") {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: error.message });
   }
-
   next(error);
 };
 
