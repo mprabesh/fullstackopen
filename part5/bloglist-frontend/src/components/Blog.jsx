028806de87f97a882e12b66531256270dd3ce97f
mprabesh <theprabesh723@gmail.com>
2023-09-02 23:49:11 +0545

    feat: blog list reformated

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
   return (
-    <div>
+    <div style={blogStyle}>
       {blog.title} {blog.author}
+      <button onClick={() => setView(!view)}>{view ? "hide" : "view"}</button>
+      <br />
+      {view ? (
+        <div>
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.user.name}
+        </div>
+      ) : null}
     </div>
   );
 };
